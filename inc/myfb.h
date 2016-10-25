4806461e529bf9b778f788bb010ea93a4cd2e7df
Alexjan Carraturo <axjslack@gmail.com>
2016-10-25 20:04:39 +0100

    I forgot what I have done
    The original modification was unknow
    
    Signed-off-by: Alexjan Carraturo <axjslack@gmail.com>

--- inc/myfb.h
+++ inc/myfb.h
@@ -6,22 +6,33 @@
 #include "stm32f4xx_hal_conf.h"
 #include "stm32f4xx_hal_ltdc.h"
 
+/* RGB 565 test 
 typedef struct strPixel
 {
-        unsigned char red: 5;
+		unsigned char red: 5;
         unsigned char green: 6;
 		unsigned char blue: 5;
+} t565Pixel;
+*/
+
+/* LTDC_PIXEL_FORMAT_ARGB8888 */
+typedef struct strPixel
+{
+	volatile uint8_t blue;
+	volatile uint8_t green;
+	volatile uint8_t red;
+	volatile uint8_t alpha;
 } t565Pixel;
 
 union Pixel
 {
-        t565Pixel  destructured[2];
+        t565Pixel  destructured;
         uint32_t structured;
 
 };
 
 typedef struct strTablePix {
-	volatile t565Pixel PixelTable[240][160];
+	volatile t565Pixel PixelTable[240][320];
 } tTablePix;
 
 
--- src/myfb.c
+++ src/myfb.c
@@ -1,7 +1,7 @@
 #include "ili9341.h"
 #include "myfb.h"
 
-static frameB myfb;
+static frameB *myfb;
 
 static void Error_Handler(void)
 {
@@ -13,154 +13,27 @@ static void Error_Handler(void)
 }
 
 
-/**
-  * @brief LCD Configuration.
-  * @note  This function Configure tha LTDC peripheral :
-  *        1) Configure the Pixel Clock for the LCD
-  *        2) Configure the LTDC Timing and Polarity
-  *        3) Configure the LTDC Layer 1 :
-  *           - direct color (RGB565) as pixel format  
-  *           - The frame buffer is located at FLASH memory
-  *           - The Layer size configuration : 240x160
-  *        4) Configure the LTDC Layer 2 :
-  *           - direct color (RGB565) as pixel format 
-  *           - The frame buffer is located at FLASH memory
-  *           - The Layer size configuration : 240x160                      
-  * @retval
-  *  None
-  */
-
-
-
-static void LCD_Config()
-{  
-  LTDC_LayerCfgTypeDef pLayerCfg;
-  //LTDC_LayerCfgTypeDef pLayerCfg1;
-
-  /* Initializaton of ILI9341 component*/
-  ili9341_Init();
-  ili9341_DisplayOn();
-  
-/* LTDC Initialization -------------------------------------------------------*/
-  
-  /* Polarity configuration */
-  /* Initialize the horizontal synchronization polarity as active low */
-  LtdcHandle.Init.HSPolarity = LTDC_HSPOLARITY_AL;
-  /* Initialize the vertical synchronization polarity as active low */ 
-  LtdcHandle.Init.VSPolarity = LTDC_VSPOLARITY_AL; 
-  /* Initialize the data enable polarity as active low */ 
-  LtdcHandle.Init.DEPolarity = LTDC_DEPOLARITY_AL; 
-  /* Initialize the pixel clock polarity as input pixel clock */  
-  LtdcHandle.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
-  
-  /* Timing configuration  (Typical configuration from ILI9341 datasheet)
-      HSYNC=10 (9+1)
-      HBP=20 (29-10+1)
-      ActiveW=240 (269-20-10+1)
-      HFP=10 (279-240-20-10+1)
-
-      VSYNC=2 (1+1)
-      VBP=2 (3-2+1)
-      ActiveH=320 (323-2-2+1)
-      VFP=4 (327-320-2-2+1)
-  */  
-
-  /* Timing configuration */
-  /* Horizontal synchronization width = Hsync - 1 */  
-  LtdcHandle.Init.HorizontalSync = 9;
-  /* Vertical synchronization height = Vsync - 1 */
-  LtdcHandle.Init.VerticalSync = 1;
-  /* Accumulated horizontal back porch = Hsync + HBP - 1 */
-  LtdcHandle.Init.AccumulatedHBP = 29;
-  /* Accumulated vertical back porch = Vsync + VBP - 1 */
-  LtdcHandle.Init.AccumulatedVBP = 3; 
-  /* Accumulated active width = Hsync + HBP + Active Width - 1 */ 
-  LtdcHandle.Init.AccumulatedActiveH = 323;
-  /* Accumulated active height = Vsync + VBP + Active Heigh - 1 */
-  LtdcHandle.Init.AccumulatedActiveW = 269;
-  /* Total height = Vsync + VBP + Active Heigh + VFP - 1 */
-  LtdcHandle.Init.TotalHeigh = 327;
-  /* Total width = Hsync + HBP + Active Width + HFP - 1 */
-  LtdcHandle.Init.TotalWidth = 279;
-  
-  /* Configure R,G,B component values for LCD background color */
-  LtdcHandle.Init.Backcolor.Blue = 0;
-  LtdcHandle.Init.Backcolor.Green = 0;
-  LtdcHandle.Init.Backcolor.Red = 0;
-
-  LtdcHandle.Instance = LTDC;
-  
-/* Layer Conf */
-  /* Windowing configuration */ 
-  pLayerCfg.WindowX0 = 0;
-  pLayerCfg.WindowX1 = 240;
-  pLayerCfg.WindowY0 = 0;
-  pLayerCfg.WindowY1 = 320;
-  
-  /* Pixel Format configuration*/ 
-  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
-  pLayerCfg.FBStartAdress = (uint32_t)&myfb.tablePix;	
-
-  pLayerCfg.Alpha = 255;
-  
-  /* Default Color configuration (configure A,R,G,B component values) */
-  pLayerCfg.Alpha0 = 0;
-  pLayerCfg.Backcolor.Blue = 0;
-  pLayerCfg.Backcolor.Green = 0;
-  pLayerCfg.Backcolor.Red = 255;
-  
-  /* Configure blending factors */
-  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_PAxCA;
-  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_PAxCA;
-  
-  /* Configure the number of lines and number of pixels per line */
-  pLayerCfg.ImageWidth = 240;
-  pLayerCfg.ImageHeight = 320;
-
-  /* Configure the LTDC */  
-  if(HAL_LTDC_Init(&LtdcHandle) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler(); 
-  }
-
-  /* Configure the Background Layer*/
-  if(HAL_LTDC_ConfigLayer(&LtdcHandle, &pLayerCfg, 0) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler(); 
-  }
-  
-  /* Configure the Foreground Layer*/
-  //if(HAL_LTDC_ConfigLayer(&LtdcHandle, &pLayerCfg1, 1) != HAL_OK)
-  //{
-  //  /* Initialization Error */
-  //  Error_Handler(); 
-  //} 
-	 
-
-}  
-
-
 
 void init_myfb()
 {
         int i,j;
 		
+		myfb=(frameB*)0xD0000000;
 
         for(i=0;i<240;i++)
         {
-                for(j=0;j<160;j++)
+                for(j=0;j<320;j++)
                 {
-				myfb.tablePix.PixelTable[i][j].red=0;
-                myfb.tablePix.PixelTable[i][j].green=0x3E;
-                myfb.tablePix.PixelTable[i][j].blue=0;
+				myfb->tablePix.PixelTable[i][j].red=0xFF;
+                myfb->tablePix.PixelTable[i][j].green=0x0;
+                myfb->tablePix.PixelTable[i][j].blue=0X0;
+				myfb->tablePix.PixelTable[i][j].alpha=0xFF;
                 }
         }
 	
 	//LCD_Config();
 	BSP_LCD_Init();
-	BSP_LCD_LayerDefaultInit(0, (uint32_t)&myfb.tableInt);
+	BSP_LCD_LayerDefaultInit(0, (uint32_t)&myfb->tableInt);
 	BSP_LCD_SetLayerVisible(0, ENABLE);
 
 }
